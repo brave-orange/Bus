d5de008c076c6376d61d10167eb23ce5a70814fa
garry wang <brave-orange@users.noreply.github.com>
2017-08-28 13:41:02 +0800

    Update JDBCConnectionFactory.java

--- src/java/com/etc/dao/JDBCConnectionFactory.java
+++ src/java/com/etc/dao/JDBCConnectionFactory.java
@@ -11,7 +11,7 @@ public class JDBCConnectionFactory {
 		Connection conn=null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/bus?useUnicode=true&characterEncoding=utf-8","root","wang");
+			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/bus?useUnicode=true&characterEncoding=utf-8","root","××××");
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
